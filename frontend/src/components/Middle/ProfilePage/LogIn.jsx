725a09383fe3020dfc9ad538403bd99558fd33c3
Code-Master20 <sahidurmiah201920@gmail.com>
2026-02-09 11:22:11 +0530

    log in page designed

--- frontend/src/components/Middle/ProfilePage/LogIn.jsx
+++ frontend/src/components/Middle/ProfilePage/LogIn.jsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import styles from "./LogIn.module.css";
 export const LogIn = () => {
+  const [loggingSuccess, setLoggingSuccess] = useState(false);
   return (
     <main className={styles["main-container-first"]}>
       <section className={styles["main-container-second"]}>
         <article className={styles["main-container-third"]}>
           <h1 className={styles["login-main-heading"]}>please log in first</h1>
           <div className={styles["login-form-container"]}>
-            <form action="#">
-              <label htmlFor="email">emali</label>
-              <input id="email" type="email" placeholder="you email" />
-              <label htmlFor="password">password</label>
-              <input
-                id="password"
-                type="password"
-                placeholder="your password"
-              />
-              <button type="submit">log-in</button>
+            <form action="#" autoComplete="off">
+              <div className={styles["input-elm"]}>
+                <label htmlFor="email">Email :</label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="your email"
+                  autoComplete="off"
+                  name="email"
+                />
+              </div>
+              <div className={styles["input-elm"]}>
+                <label htmlFor="password">Password :</label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="your password"
+                  autoComplete="off"
+                  name="password"
+                />
+              </div>
+              <div>
+                <button type="submit">log-in</button>
+                {!loggingSuccess && <button>sign-up</button>}
+              </div>
             </form>
           </div>
         </article>
