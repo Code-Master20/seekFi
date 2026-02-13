2a18b7ddb181d37f592e6b9377cc07265c7b99b7
Code-Master20 <sahidurmiah201920@gmail.com>
2026-02-13 19:52:45 +0530

    isMe restructured

--- backend/middlewares/expressMiddleware/isMe.middleware.js
+++ backend/middlewares/expressMiddleware/isMe.middleware.js
@@ -3,19 +3,24 @@ const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../../utils/errorHandler.util");
 
 const isMeMiddleware = (req, res, next) => {
-  const token = req.cookies?.token;
-
-  if (!token) {
-    return new ErrorHandler(401, "Not authenticated").send(res);
-  }
-
   try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return new ErrorHandler(401, "Not authenticated").send(res);
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_LOGGED_TRACK_SECRET_KEY);
 
-    req.user = decoded; // payload from JWT
+    if (!decoded) {
+      return ErrorHandler(401, "session expired please log-in again").send(res);
+    }
+    req.user = decoded; // payload from JWT {only email, id}
     next();
   } catch (err) {
-    return new ErrorHandler(401, "Invalid or expired token").send(res);
+    return new ErrorHandler(
+      500,
+      "Internal server Error. Please refresh or Log-in again ",
+    ).send(res);
   }
 };
 
