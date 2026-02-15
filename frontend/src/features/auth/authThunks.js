10aac1feb72a3d26fbdca205b5a91c8f67c2f1aa
Code-Master20 <sahidurmiah201920@gmail.com>
2026-02-16 00:30:53 +0530

    utils add for hitting backend url

--- frontend/src/features/auth/authThunks.js
+++ frontend/src/features/auth/authThunks.js
@@ -1,16 +1,11 @@
+import api from "../../utils/api";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-// http://localhost:5173
-// https://seekfi.onrender.com
 //========================tracking if user is already logged in==========================
 export const checkMe = createAsyncThunk("auth/isMe", async (_, thunkAPI) => {
   try {
-    const response = await axios.get(
-      "https://seekfi.onrender.com/api/auth/me",
-      {
-        withCredentials: true,
-      },
-    );
+    const response = await api.get("/api/auth/me", {
+      withCredentials: true,
+    });
 
     return response.data;
   } catch (error) {
@@ -45,11 +40,7 @@ export const signUpOtpReceived = createAsyncThunk(
   "auth/signUpOtp",
   async (clientCredentials, thunkAPI) => {
     try {
-      const response = await axios.post(
-        "https://seekfi.onrender.com/api/auth/sign-up",
-        clientCredentials,
-        // { withCredentials: true },
-      );
+      const response = await api.post("/api/auth/sign-up", clientCredentials);
 
       console.log(response.data);
       return response.data;
@@ -62,15 +53,11 @@ export const otpVerifiedAndSignedUp = createAsyncThunk(
   "auth/verifiedOtp",
   async (clientCredentials, thunkAPI) => {
     try {
-      const response = await fetch(
-        `https://seekfi.onrender.com/api/auth/sign-up/verify-otp`,
+      const response = await api.post(
+        "/api/auth/sign-up/verify-otp",
+        clientCredentials,
         {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(clientCredentials),
-          credentials: "include",
+          withCredentials: true,
         },
       );
 
