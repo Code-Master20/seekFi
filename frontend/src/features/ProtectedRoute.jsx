4714850051d08705172b6837846038d97ee5d287
Code-Master20 <sahidurmiah201920@gmail.com>
2026-02-19 00:22:19 +0530

    Navigate imported

--- frontend/src/features/ProtectedRoute.jsx
+++ frontend/src/features/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useSelector((state) => state.auth);
